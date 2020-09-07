2d518a46951ce998a4fe3a3437d27f4bf7727256
Ture Thörnvik <tthornvik@deloittedigital.se>
2020-09-07 15:07:13 +0200

    excercise complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React, { useState, ChangeEvent } from "react";
-import "./App.css";
+import "./App.scss";
 
 import Input from "./components/input/Input";
 import Output from "./components/output/Output";
 
 function App() {
   const [name, setName] = useState("hello World");
+  const [username, setUsername] = useState("Ture");
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     setName(event.target.value);
@@ -14,12 +15,12 @@ function App() {
   return (
     <div className="App">
       <Output
-        name="Why hello there"
-        paragraph="I'm baby hexagon bicycle rights enamel pin butcher slow-carb. Direct trade lo-fi flannel typewriter. Retro small batch mumblecore tumblr biodiesel kale chips poke freegan semiotics keffiyeh keytar cold-pressed prism. Iceland twee lomo quinoa tofu celiac. Direct trade farm-to-table gastropub, lumbersexual pickled seitan hexagon poutine unicorn. Bespoke farm-to-table pitchfork keytar hot chicken before they sold out readymade quinoa, banjo venmo lo-fi fam put a bird on it. Selfies austin everyday carry, brooklyn vexillologist hot chicken bespoke."
+        name={"Why hello there... " + username}
+        paragraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eleifend sagittis mauris, id euismod leo ornare vel. Praesent fringilla sapien nec elit pulvinar, et finibus nisl porttitor. Etiam tincidunt ipsum at tincidunt porta. "
       ></Output>
       <Output
         name={name}
-        paragraph="out but above will display what you type in inputfield"
+        paragraph="textfield above will output what you type in the inputfield!"
       ></Output>
       <Input name={name} onChange={handleChange}></Input>
     </div>
