abfafdf1d860f2a19ecfe543a72d62b9aec637d4
Ture Thörnvik <tthornvik@deloittedigital.se>
2020-09-07 14:07:36 +0200

    import sass to input/output

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Input from "./components/input/Input";
 import Output from "./components/output/Output";
 
 function App() {
-  const [name, setName] = useState("hello");
+  const [name, setName] = useState("hello World");
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     setName(event.target.value);
@@ -13,8 +13,14 @@ function App() {
 
   return (
     <div className="App">
-      <Output name="ture" paragraph="hipster ipsum"></Output>
-      <Output name={name} paragraph="hipster ipsum"></Output>
+      <Output
+        name="Why hello there"
+        paragraph="I'm baby hexagon bicycle rights enamel pin butcher slow-carb. Direct trade lo-fi flannel typewriter. Retro small batch mumblecore tumblr biodiesel kale chips poke freegan semiotics keffiyeh keytar cold-pressed prism. Iceland twee lomo quinoa tofu celiac. Direct trade farm-to-table gastropub, lumbersexual pickled seitan hexagon poutine unicorn. Bespoke farm-to-table pitchfork keytar hot chicken before they sold out readymade quinoa, banjo venmo lo-fi fam put a bird on it. Selfies austin everyday carry, brooklyn vexillologist hot chicken bespoke."
+      ></Output>
+      <Output
+        name={name}
+        paragraph="out but above will display what you type in inputfield"
+      ></Output>
       <Input name={name} onChange={handleChange}></Input>
     </div>
   );
--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import styles from "./Input.module.sass";
+
 interface InputProps {
   name: string;
   onChange: any;
@@ -7,7 +9,7 @@ interface InputProps {
 
 const Input = ({ name, onChange }: InputProps) => {
   return (
-    <div>
+    <div className={styles.inputContainer}>
       <input type="text" value={name} onChange={onChange} />
     </div>
   );
